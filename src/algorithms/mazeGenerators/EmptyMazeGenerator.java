186ac3454a3afcaea7c7409f19aee24b47ce598d
omrihershko <omrihers@post.bgu.ac.il>
2024-06-01 20:01:52 +0300

    i make space

--- src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -5,7 +5,7 @@ public class EmptyMazeGenerator extends AMazeGenerator {
 
     @Override
     public Maze generate(int rows, int columns) {
-        Maze maze_simple = new Maze(0, 0);
+         Maze maze_simple = new Maze(0, 0);
         return maze_simple;
     }
 }
