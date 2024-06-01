98b7c916133a917db54256e2859f488eb6039129
Owner <guyamzaleg@gmail.com>
2024-06-01 20:49:32 +0300

    ENTER ADDED

--- src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -4,7 +4,7 @@ public class EmptyMazeGenerator extends AMazeGenerator {
 
 
 
-
+//ADDED BY ME
     @Override
     public Maze generate(int rows, int columns) {
          Maze maze_simple = new Maze(0, 0);
