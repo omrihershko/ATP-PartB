7aeff0befe6e5d177cf52c121368e04f9f7e5648
Owner <guyamzaleg@gmail.com>
2024-07-09 19:30:24 +0300

    My Compress and Decompress-Finished

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -11,60 +11,36 @@ public class MyCompressorOutputStream extends OutputStream {
         this.out = out;
     }
     @Override
-    public void write(int b) {
+    public void write(int b) throws IOException {
+        this.out.write(b);
     }
 
     @Override
-    // This function compress the byte array that represent the maze to a smaller byte array that represent the maze
-    // The first 12 bytes are the maze details , the rest  are the maze cells
-    // The function compress the maze cells from 8 bytes to one byte
-    //because each cell can be 0 or 1, so we can represent 8 cells in one byte
-    public void write(byte[] b) throws IOException {
-        ArrayList<Byte> result = new ArrayList<>();
-        int current = 0;
-        byte[] array_of_8 = new byte[8];
-        for (int i = 0; i < b.length; i++) {
-            if (i < 12)
+
+    // 24 bytes for the maze details
+    //insert the 1's indexes into the stream
+    //creating a stream of 1's indexes representing the maze
+    public void write(byte[] bytes) throws IOException
+    {
+        int count_for_256 = 0;
+        for (int i=0;i< bytes.length;i++)
+        {
+            count_for_256 = i/256;
+            byte b = bytes[i];
+            if(i<24)
             {
-                // Write the first 10 bytes as is
-                // The first 10 bytes are the maze details
-                result.add(b[i]);
+                write(b);
                 continue;
             }
-            //create a byte array of 8 cells and turn it to a single byte
-            array_of_8[current] = b[i];
-            current++;
-            if (current == 8) {
-                // Write the compressed byte
-                // The compressed byte is the 8 cells in one byte
-                result.add(byte_arr_to_byte_num(array_of_8));
-                // Reset the array of 8 cells
-                current = 0;
-            }
-            // If the last byte is not a full byte
-            // Fill the rest of the byte with 0
-            if (i+1 == b.length && current != 0)
+            //if the byte is 1, insert its index into the stream
+
+            if(b==1)
             {
-                while(current < 8)
-                {
-                    array_of_8[current] = 0;
-                    current++;
-                }
-                result.add(byte_arr_to_byte_num(array_of_8));
+                write(i-count_for_256*256);
             }
+
         }
-        // Write the compressed maze to the output stream
-        for (Byte B : result)
-        {
-            out.write(B);
-        }
-    }
-    private byte byte_arr_to_byte_num(byte[] arr) {
-        int result = 0;
-        for (int i = 7; i >= 0; i--) {
-            result += (arr[i]) * (int) Math.pow(2, 7 - i);
-        }
-        return (byte) (result - 128);
+
     }
 
 
--- src/test/RunCompressDecompressMaze.java
+++ src/test/RunCompressDecompressMaze.java
@@ -14,16 +14,16 @@ import java.util.Arrays;
 public class RunCompressDecompressMaze {
     public static void main(String[] args)
     {
-        for (int i = 0; i < 10; i++) {
-            String mazeFileName = "Maze.maze";
+//        for (int i = 0; i < 10; i++) {
+            String mazeFileName = "savedMaze.maze";
             AMazeGenerator mazeGenerator = new MyMazeGenerator();
-            Maze maze = mazeGenerator.generate(10, 10); //Generate new maze
+            Maze maze = mazeGenerator.generate(100, 100); //Generate new maze
             maze.print();
             System.out.println();
             try {
                 // save maze to a file
-                OutputStream out = new SimpleCompressorOutputStream(new FileOutputStream(mazeFileName));
-//            OutputStream out = new MyCompressorOutputStream(System.out);
+//                OutputStream out = new SimpleCompressorOutputStream(new FileOutputStream(mazeFileName));
+            OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileName));
                 out.write(maze.toByteArray());
                 out.flush();
                 out.close();
@@ -33,8 +33,8 @@ public class RunCompressDecompressMaze {
             byte savedMazeBytes[] = new byte[0];
             try {
                 //read maze from file
-//          InputStream in = new MyDecompressorInputStream(new FileInputStream(mazeFileName));
-                InputStream in = new SimpleDecompressorInputStream(new FileInputStream(mazeFileName));
+          InputStream in = new MyDecompressorInputStream(new FileInputStream(mazeFileName));
+//                InputStream in = new SimpleDecompressorInputStream(new FileInputStream(mazeFileName));
                 savedMazeBytes = new byte[maze.toByteArray().length];
                 in.read(savedMazeBytes);
                 in.close();
@@ -46,8 +46,8 @@ public class RunCompressDecompressMaze {
             loadedMaze.print();
             boolean areMazesEquals = Arrays.equals(loadedMaze.toByteArray(),maze.toByteArray());
             System.out.println(String.format("Mazes equal: %s",areMazesEquals));
-            //maze should be equal to loadedMaze
-        }
-
+//            maze should be equal to loadedMaze
     }
+
+//    }
 }
